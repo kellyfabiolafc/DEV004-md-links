74a41a67617b973b212134ba9f309d41e3145213
Kelly fabiola <floresdelacruzkellyfabiola@gmail.com>
2023-05-08 21:00:05 -0500

    Funciona , pero aun no se por que

---
+++ ARCHIVO.md
@@ -0,0 +1,3 @@
+[Markdown](https://es.wikipedia.org/wiki/Markdown)
+[Node.js](https://nodejs.org/)
+ [Manual Mocks con Jest - Documentación oficial](https://jestjs.io/docs/es-ES/manual-mocks)
--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-module.exports = () => {
-  // ...
-};
+import { mdLinks } from "./mdlinks.js";
+import chalk from 'chalk';
+
+
+const ruta = './DEV004-MD-LINKS';
+const opciones = { validate: true };
+
+
+console.log(); // línea vacía
+console.log(chalk.hex('#FF69B4').bold('\t\t\t\t\t     Md Links \n'));
+console.log(chalk.hex('#A9D6E5').bold('\t\t\t\t\t De Fabiola Flores \n\n'));
+//
+mdLinks(ruta, opciones)
+  .then((links) => console.log(links))
+  .catch((err) => console.error(err));
